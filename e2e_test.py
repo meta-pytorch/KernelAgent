706dc75dbcf97bc5eb5ed1b143e3b0499b2b6aa0
Laura Wang <3700467+Laurawly@users.noreply.github.com>
2025-10-09 20:03:45 -0700

    Add Automated Repo Checkup files (#17)
    * added non zero anticheating to testgen
    
    * added CODE_OF_CONDUCT
    
    * added CONTRIBUTING.md
    
    * add header
    
    * added header to prompts

--- e2e_test.py
+++ e2e_test.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
-"""
-End-to-end test for BF16 matmul kernel with Sigmoid activation fused.
-M = 1024, N = 2058, K = 4096
-"""
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""End-to-end BF16 matmul+sigmoid test harness."""
 
 import sys
 import time
--- triton_kernel_agent/prompt_manager.py
+++ triton_kernel_agent/prompt_manager.py
@@ -1,9 +1,18 @@
-"""
-Prompt Manager for handling Jinja2 templates
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-This module provides centralized template loading and rendering functionality
-for all prompts used in the Triton kernel generation system.
-"""
+"""Prompt Manager for handling Jinja2 templates."""
 
 from pathlib import Path
 from typing import Dict, Optional
