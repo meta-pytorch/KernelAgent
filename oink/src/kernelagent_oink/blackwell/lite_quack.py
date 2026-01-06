c34cc12ca8fa8298a6fcde5588bf5dc2b4495df3
Laura Wang <3700467+Laurawly@users.noreply.github.com>
2026-01-06 12:25:31 -0800

    Fix oink ruff lint and add license headers

--- oink/src/kernelagent_oink/blackwell/lite_quack.py
+++ oink/src/kernelagent_oink/blackwell/lite_quack.py
@@ -1,3 +1,17 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Lightweight local clone of the small subset of helpers that the SM100
 RMSNorm CuteDSL kernels depend on.
@@ -12,9 +26,8 @@ from __future__ import annotations
 
 import math
 import operator
-from typing import Callable, Optional, Tuple
+from typing import Callable, Optional
 
-import cuda.bindings.driver as cuda  # type: ignore
 import torch
 from torch import Tensor
 
@@ -23,7 +36,7 @@ import cutlass.cute as cute
 from cutlass import Float32, Int32, const_expr
 from cutlass.cute.runtime import from_dlpack
 from cutlass.cutlass_dsl import T, dsl_user_op
-from cutlass._mlir.dialects import llvm, nvvm, vector
+from cutlass._mlir.dialects import llvm
 
 
 # -------------------------
@@ -347,4 +360,3 @@ def get_sm_count(N: int, device: torch.device) -> int:
         sm_count * sm_count_multiple if N <= 8192 else sm_count // 2 if N <= 16384 else sm_count * 2
     )
     return sm_count
-
