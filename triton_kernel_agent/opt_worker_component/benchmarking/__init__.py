2994315671d6f88af9b4be739826945292d9656f
Kaiming Cheng <kaimingcheng@fb.com>
2026-01-09 11:49:12 -0800

    fix ruff

--- triton_kernel_agent/opt_worker_component/benchmarking/__init__.py
+++ triton_kernel_agent/opt_worker_component/benchmarking/__init__.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Benchmarking infrastructure for kernel performance measurement.
-
-"""
+"""Benchmarking infrastructure for kernel performance measurement."""
 
 # Core benchmarking
 from .benchmark import Benchmark, BenchmarkLockManager
